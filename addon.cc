030a57b18cc6e3faa42c4dad5d7d46c5a453d605
onassau <85627253+onassau@users.noreply.github.com>
2021-06-08 16:11:31 +0200

    Add addNumbers in addon, compare exec time with js

--- addon.cc
+++ addon.cc
@@ -1,31 +1,49 @@
 #include <node.h>
 #include <iostream>
 
-using namespace v8;
-using namespace std;
+namespace demo
+{
+	using v8::FunctionCallbackInfo;
+	using v8::Isolate;
+	using v8::Context;
+	using v8::Exception;
+	using v8::Local;
+	using v8::Object;
+	using v8::String;
+	using v8::Value;
 
-void HelloWorld(const FunctionCallbackInfo<Value>& args) {
-   cout << "Hello, world!" << endl;
-}
+	void HelloWorld(const FunctionCallbackInfo<Value>& args) {
+	   std::cout << "Hello, world!" << std::endl;
+	}
 
-void AddNumbers(const FunctionCallbackInfo<Value>& args) {
-   Isolate* isolate = args.GetIsolate();
-   double valueToSum = args[0]->NumberValue();
-   double result = 0;
-   int sumCount = args[1]->IntegerValue();
-   int i;
+	void AddNumbers(const FunctionCallbackInfo<Value>& args) {
+	   Isolate* isolate = args.GetIsolate();
+	   Local<Context> context = isolate->GetCurrentContext();
+	   
+	   //if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+	   //   isolate->ThrowException(Exception::TypeError(
+	   //        String::NewFromUtf8(isolate, "Wrong arguments").ToLocalChecked()));
+	   //   return;
+	   //}
+	   
+	   double valueToSum = args[0]->NumberValue(context).FromJust();
+	   double result = 0;
+	   int sumCount = args[1]->IntegerValue(context).FromJust();
+	   int i;
 
-   for (i = 0; i < sumCount; i++) {
-       result = result + valueToSum;
-   }
+	   for (i = 0; i < sumCount; i++) {
+		   result += valueToSum;
+	   }
 
-   args.GetReturnValue().Set(result);
-}
+	   args.GetReturnValue().Set(result);
+	}
 
 
-void Initialize(Local<Object> exports) {
-   NODE_SET_METHOD(exports, "helloWorld", HelloWorld);
-   NODE_SET_METHOD(exports, "addNumbers", AddNumbers);
-}
+	void Initialize(Local<Object> exports) {
+	   NODE_SET_METHOD(exports, "helloWorld", HelloWorld);
+	   NODE_SET_METHOD(exports, "addNumbers", AddNumbers);
+	}
 
-NODE_MODULE(addon, Initialize);
+	NODE_MODULE(addon, Initialize);
+
+} // namespace demo
--- my_app.js
+++ my_app.js
@@ -1,4 +1,27 @@
-const addon = require('./build/Release/addon');
-const runAddon = () => addon.helloWorld();
+const addon = require('./build/Release/addon.node');
 
-runAddon();
+const addNumbersAddon = (num, count) =>
+	addon.addNumbers(num, count);
+
+const addNumbersNode = (num, count) =>
+	{
+	 let result = 0;
+	 for (let i = 0; i < count; i++) {
+	   result = result + num;
+	 }
+	 return result;
+	};
+
+const addNumbers = (number, addingTimes) => {
+	console.time('C++');
+	console.log(addNumbersAddon(number, addingTimes));
+	console.timeEnd('C++');
+
+	console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+
+	console.time('JS');
+	console.log(addNumbersNode(number, addingTimes));
+	console.timeEnd('JS');
+};
+
+addNumbers(3.14, 100000000);
